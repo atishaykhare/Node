a3855a91dc5268252fd72139f9cd19464425e148
atishaykhare <atishaykhare123@gmail.com>
2022-05-01 23:39:08 +0530

    Handling Response

--- Rotuing.js
+++ Rotuing.js
@@ -6,25 +6,34 @@ function reqListener(req, res) {
     const url = req.url;
     const method = req.method;
 
-    if(url === '/'){
+    if (url === '/') {
         res.write('<html>')
         res.write('<head><title>Input Page</title></head>')
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><br/><button type="submit" >Submit</button> </form></body>')
         res.write('</html>')
         return res.end();
     }
-    if(url === '/message' && method === 'POST'){
-        fs.writeFileSync('dummy.txt', 'Dummy Data')
-        res.statusCode = 302;
-        res.setHeader('Location', '/')
-        console.log({req})
-        return res.end();
-        res.write('<html>')
-        res.write('<head><title>Input Page</title></head>')
-        res.write('<body><h1></h1></body>')
-        res.write('</html>')
-        return res.end();
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log({chunk})
+            body.push(chunk);
+        })
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('dummy.txt', message)
+            res.statusCode = 200;
+            res.write('<html>')
+            res.write('<head><title>Input Page</title></head>')
+            res.write(`<body><h1>${message}</h1></body>`)
+            res.write('</html>')
+            return res.end();
+        })
     }
+    res.statusCode = 302;
+    res.setHeader('Location', '/')
+    return res.end();
 
 }
 
